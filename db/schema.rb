49f5cd5f295d8bfc7cb6881d368ba6329c48b063
Kappalucky <Shaquille.j.foster@gmail.com>
2017-08-02 05:18:23 +0000

    Updated Rails and Rake, Created contact form view

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170802024806) do
+ActiveRecord::Schema.define(version: 20170802050804) do
 
-  create_table "contacts", force: true do |t|
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
